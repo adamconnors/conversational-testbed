13a72eb33091696f492aadae1d4ac713610a2d4e
adamconnors@gmail.com <adamconnors@google.com>
2024-04-23 10:41:02 +0100

    Add unit-tests for history tutor and update prompt.

--- conversational-client/src/app/components/history-tutor/history-tutor.component.ts
+++ conversational-client/src/app/components/history-tutor/history-tutor.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrl: './history-tutor.component.css',
 })
 export class HistoryTutorComponent {
-  worldState: WorldState | undefined = undefined;
+  worldState: WorldState | undefined;
 
   getInitialWorldState(): object[] {
     return [];
@@ -14,19 +14,26 @@ export class HistoryTutorComponent {
 
   // Called when the worldstate is returned from the server.
   updateWorldState(state: object[]): object[] {
-    console.log('Got updated world state');
-    console.log(state);
-    const questions: Question[] = [];
-    for (const question of state) {
-      questions.push(question as Question);
-    }
-    this.worldState = {questions: questions};
+    this.worldState = { questions: state as Question[] };
 
     // TODO: I'm converting into an object model so I can render if more cleanly,
     // but the object model is specific to each module so I end up returning the original
     // JSON object. If the client ends up needing to update the world state this will mean
     // converting it and then converting it back. I need to find a better way to do this.
     return state;
+  }
+
+  // TODO: This is because sometimes the model changes the name of the field. I need to
+  // use pydantic instead so this is consistent.
+  hasAnswered(answer: Answer) {
+    if (answer.hasAnswered === true || answer.has_answered === true) {
+      return true;
+    }
+    if (answer.hasAnswered === 'true' || answer.has_answered === 'true') {
+      return true;
+    }
+
+    return false;
   }
 
   calculatePercentageComplete() {
@@ -40,7 +47,7 @@ export class HistoryTutorComponent {
     for (const question of this.worldState!.questions) {
       for (const answer of question.answers) {
         totalAnswers += 1;
-        if (answer.hasAnswered == true) {
+        if (this.hasAnswered(answer)) {
           correctAnswers += 1;
         }
       }
@@ -61,7 +68,9 @@ interface Question {
   answers: Answer[];
 }
 
+// Interface is needed since LLM object is not always consistent.
 interface Answer {
   answer: string;
-  hasAnswered: boolean;
+  hasAnswered: boolean | string;
+  has_answered: boolean | string;
 }
