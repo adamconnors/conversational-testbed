2a2d43824be5a6ca5aecf99816c35ce19c943de1
adamconnors@gmail.com <adamconnors@google.com>
2024-04-29 12:49:28 +0100

    Fixing up history tutor
    Simpler prompt.
    Test validations don't run in parallel yet.

--- py-server/history_tutor/history_tutor.py
+++ py-server/history_tutor/history_tutor.py
@@ -7,31 +7,18 @@ from langchain_core.output_parsers import JsonOutputParser
 
 # set parent directory path
 sys.path.append("../py-server")
-from agents import ConversationalAgent
+from agents import AgentResponse, ConversationalAgent
 import os
 
 
 CONVERSATION_PROMPT = """
-    You are an expert AUDIO chatbot designed to teach GCSE History.
+    You are an expert AUDIO chatbot designed to provide a history quiz to GCSE students.
     
-    This is the history lesson plan.:
+    This is the set of questions to ask and whether or not the student has asked them.:
     ```
     {lesson_context}
     ```
     
-    The JSON below represents all the questions to ask. Each question has multiple parts to the answer.
-    The hasAnswered flag indicates whether the student has already answered this question or not.
-    
-    ```
-    {world_state}
-    ```
-    
-    This is the chat history so far:
-    {chat_history}
-    
-    This is the last message sent by the student:
-    {last_message}
-
     Consider each of these possibilities and give the most appropriate response:
 
     1. If this is the start of the lesson, give a one sentence introduction to the topic and then ask the first question.
@@ -50,50 +37,8 @@ CONVERSATION_PROMPT = """
     way and prompt the user for more relevant information.
 """
 
-INITIAL_WORLD_STATE_PROMPT = """
-    You are a history tutor. Based on this lesson plan, identify the main areas you'll be asking
-    questions about and organize them into a list. Include an attribute has_answered which is always false.
-    
-    Some questions have multiple parts to the answer, list all answers.
-    
-    This is the lesson plan:
-    ```
-    {lesson_context}
-    ```
-    
-    Return results as an array of JSON objects.   
-    [
-        {{
-            "question": "What were the different theories about the cause of the Black Death?",
-            "answers": [
-            {{"answer": "Religion: God sent the plague as a punishment for people's sins.", "hasAnswered": "false"}},
-            {{"answer": "Miasma: bad air or smells caused by decaying rubbish.", "hasAnswered": "false"}},
-            {{"answer": "Four Humours: most physicians believed that disease was caused by an imbalance in the Four Humours.", "hasAnswered": "false"}},
-            {{"answer": "Outsiders: strangers or witches had caused the disease.", "hasAnswered": "false"}}
-            ]
-        }}
-    ]
-"""
-
-UPDATE_WORLD_STATE_PROMPT = """
-You are a history tutor testing a student. The JSON below represents all the
-questions they need to answer and whether or not that have answered.
-
-```
-{world_state}
-```
-
-Based on the previous chat history with the student and their last message, update the world state to reflect
-any questions they have answered.
-
-Chat history: {chat_history}
-Last message: {last_message}
-
-Return results as an array of JSON objects. Return ALL answers in the original world state.  
-"""
-
 BLACK_DEATH_TUTOR_CONTEXT = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "the_black_death.md"
+    os.path.dirname(os.path.abspath(__file__)), "the_black_death.json"
 )
 
 
@@ -104,51 +49,18 @@ def load_file(filename):
 
 class HistoryTutor(ConversationalAgent):
     def __init__(self):
-        start = time.time()
-        self.gemini = VertexAI(model_name="gemini-pro")
         self.chat_model = ChatVertexAI(
             model="gemini-pro", convert_system_message_to_human=True
         )
-        end = time.time()
-        print(f"HistoryTutor loaded in {end - start:0.2f} seconds")
 
-        self.lesson_context = load_file(BLACK_DEATH_TUTOR_CONTEXT)
+        self.lesson_context = json.loads(load_file(BLACK_DEATH_TUTOR_CONTEXT))
 
-    def chat(self, message: str):
-        response = self.chat_model.invoke(self._state.message_history)
-        return response.content
-
-    def _build_system_prompt(self, state):
-        return PromptTemplate.from_template(CONVERSATION_PROMPT).format(
-            lesson_context=self.lesson_context,
-            last_message=state.message,
-            chat_history=state.message_history,
-            world_state=json.dumps(state.world_state),
-        )
-
-    def _init_world_state(self):
-        prompt = PromptTemplate.from_template(INITIAL_WORLD_STATE_PROMPT).format(
-            lesson_context=self.lesson_context
-        )
-        response = self.gemini.invoke(prompt)
-        return JsonOutputParser().parse(response)
-
-    def _build_world_state(self, state):
-        world_state = None
-        if not state.world_state:
-            print("No world state received, creating new one")
-            world_state = self._init_world_state()
-            print("World state created", world_state)
-            return world_state
-
-        prompt = PromptTemplate.from_template(UPDATE_WORLD_STATE_PROMPT).format(
-            world_state=json.dumps(state.world_state),
-            chat_history=state.message_history,
-            last_message=state.message,
-        )
-        try:
-            response = self.gemini.invoke(prompt)
-            return JsonOutputParser().parse(response)
-        except Exception as e:
-            print("Couldn't parse world_state", e)
-            return {"failed": True}
+    def chat(self, agent_state) -> AgentResponse:
+        promptTemplate = self._from_messages(CONVERSATION_PROMPT, agent_state)
+        
+        # Any JSON added to the prompt needs to be escaped.
+        escaped_lesson_context = json.dumps(self.lesson_context).replace("{", "{{").replace("}", "}}")     
+        prompt = promptTemplate.invoke({"lesson_context": escaped_lesson_context})
+        
+        response = self.chat_model.invoke(prompt)
+        return (response.content, self.lesson_context)
--- py-server/test_utils.py
+++ py-server/test_utils.py
@@ -1,6 +1,8 @@
 from typing import List
 from agents import MessageHistory
 from langchain_core.messages import AIMessage, HumanMessage
+import tabulate
+import concurrent.futures
 
 def build_message_history_for_test(messages: List[str]) -> MessageHistory:
     """
@@ -15,3 +17,33 @@ def build_message_history_for_test(messages: List[str]) -> MessageHistory:
         else:
             rtn.append(AIMessage(message))
     return rtn
+
+
+def run_in_parallel(evaluator, times):
+        # Create a list to store the results
+        results = []
+        responses = []
+
+        # Create a ThreadPoolExecutor with 10 threads
+        with concurrent.futures.ThreadPoolExecutor(max_workers=times) as executor:
+            for _ in range(times):
+                future = executor.submit(evaluator)
+                results.append(future)
+        # Wait for all the results to complete
+        concurrent.futures.wait(results)
+        return results
+
+
+def tabulate_evaluator_results(responses, results):
+    table = []
+    passed = 0
+    for i, result in enumerate(responses):
+        score = results[i]["score"]
+        reasoning = results[i]["reasoning"]
+        if (score < 1.0):
+            table.append([result, score, reasoning])
+        else:
+            passed += 1
+        
+    print(f"Passed: {passed} out of {len(responses)}")
+    print(tabulate.tabulate(table, headers=["Response", "Score", "Rationale"]))
