094a48e52132a421b9d30f5f1723d6825ee817ee
alalamav <2132122+alalamav@users.noreply.github.com>
2024-06-04 00:14:17 -0400

    lazy load agents (#34)

--- py-server/agents/registry.py
+++ py-server/agents/registry.py
@@ -1,4 +1,4 @@
-from typing import Dict, Final
+from typing import Dict, Final, Type
 from .agents import ConversationalAgent
 from .default_agent import DefaultAgent
 from .fake_agent import FakeAgent
@@ -8,8 +8,46 @@ from .history_tutor.history_tutor import HistoryTutor
 # It's able to respond to user messages based on the conversation history
 # and previous state.
 # New agents should be registered here.
-AGENT_BY_ID: Final[Dict[str, ConversationalAgent]] = {
-    "default": DefaultAgent(),
-    "fake": FakeAgent(),
-    "history_tutor": HistoryTutor(),
+_AGENT_BY_ID: Final[Dict[str, Type[ConversationalAgent]]] = {
+    "default": DefaultAgent,
+    "fake": FakeAgent,
+    "history_tutor": HistoryTutor,
 }
+
+# Dict to keep track of instantiated agents.
+_ACTIVE_AGENT_BY_ID: Final[Dict[str, Type[ConversationalAgent]]] = {}
+
+
+class AgentRegistry:
+    """A registry for ConversationalAgents.
+    This class provides a way to retrieve ConversationalAgents by id.
+    """
+
+    def get_agent(self, agent_id: str) -> ConversationalAgent:
+        """Retrieves a ConversationalAgent by id. Instantiates agent obejcts
+        lazily.
+
+        Args:
+            agent_id: The id of the agent to retrieve.
+        Returns:
+            The ConversationalAgent object.
+        Raises:
+            ValueError: If the agent id is not registered.
+        """
+        if agent_id not in _AGENT_BY_ID:
+            raise ValueError(f"agent '{agent_id}' not registered.")
+
+        if agent_id in _ACTIVE_AGENT_BY_ID:
+            return _ACTIVE_AGENT_BY_ID[agent_id]
+
+        return _AGENT_BY_ID[agent_id]()
+
+    def is_agent_registered(self, agent_id: str) -> bool:
+        """Checks if an agent is registered.
+
+        Args:
+            agent_id: The id of the agent to check.
+        Returns:
+            True if the agent is registered, False otherwise.
+        """
+        return agent_id in _AGENT_BY_ID
