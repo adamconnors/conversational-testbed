42ab476e7b504a42d769e493c0ee53163fd64cc6
adamconnors <adamconnors@google.com>
2024-06-17 14:34:44 +0100

    Add a physics-agent for use with a blog post about voice (#37)
    * First cut of a chat client to aid debugging of new agents.
    
    * reformat
    
    * First cut at a physics expert agent.

--- py-server/agents/physics_expert/physics_expert.py
+++ py-server/agents/physics_expert/physics_expert.py
@@ -2,8 +2,10 @@ import time
 from langchain_google_vertexai import ChatVertexAI
 from agents.agents import AgentResponse, ConversationalAgent
 import os
-from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from pprint import pprint
 
 SYSTEM_PROMPT = """
 Create a concise and clear audio chatbot that engages in a voice-in, voice-out conversation about a complex physics concept in order to help the user develop a better intuitive understanding of the subjet.
@@ -23,24 +25,28 @@ Don't use phrases like: "You're absolutely right", instead focus on the scientif
 Don't ask any questions, let the user drive the conversation.
 
 ==
-Use this information to create your answers:
+Use this information when answering user questions:
 {articles}
 ==
-
-EXAMPLE CONVERSATION:
-User: Tell me about the double-slit experiment.
-AI: The double-slit experiment demonstrates the wave-particle duality of matter. Basically, a beam of particles (such as electrons) is shone on a screen that has two slits cut out, creating
-two separate pathways. Then you observe the pattern formed by the particles on another screen behind the first one.   What do you expect to happen?
-User: How does the double slit experiment work?
-AI: In the double slit experiment, particles are fired at a barrier with two slits. The particles create an interference pattern on the screen behind the barrier, showing wave-like behavior.
-User: Ok, what makes us think they're particles?
-AI: We can detect individual particles hitting the screen, which shows that they behave like particles. However, the interference pattern suggests wave-like behavior.
-
-==
-ACTUAL CONVERSATION:
 """
 
-# Drop content into this directory to add it to the prompt.
+EXAMPLES = [
+    HumanMessage("Tell me about the double-slit experiment."),
+    AIMessage("Aye! That be a fair old tricky question that be."),
+    #    AIMessage(
+    #        "The double-slit experiment demonstrates the wave-particle duality of matter. #Basically, a beam of particles (such as photons or electrons) is shone on a screen that has two #slits cut out, creating two separate pathways. Then you observe the pattern formed by the #particles on another screen behind the first one. What do you expect to happen?"
+    #    ),
+    HumanMessage("How does the double slit experiment work?"),
+    AIMessage(
+        "In the double slit experiment, particles are fired at a barrier with two slits. The particles create an interference pattern on the screen behind the barrier, showing wave-like behavior."
+    ),
+    HumanMessage("Ok, what makes us think they're particles?"),
+    AIMessage(
+        "We can detect individual particles hitting the screen, which shows that they behave like particles. However, the interference pattern suggests wave-like behavior."
+    ),
+]
+
+# Drop content into this directory to add it to the prompt for more in-depth answers.
 RESOURCES = os.path.join(os.path.dirname(os.path.abspath(__file__)), "content")
 
 
@@ -53,18 +59,31 @@ def load_resources():
                 all_text += file.read()
     return all_text
 
+
 class PhysicsExpert(ConversationalAgent):
     def __init__(self):
 
-        self.article_content = load_resources()
+        article_content = load_resources()
+
+        self.system_context = PromptTemplate.from_template(
+            template=SYSTEM_PROMPT
+        ).format(articles=article_content)
 
         self.chat_model = ChatVertexAI(
-            model="gemini-1.5-flash-001", convert_system_message_to_human=False, max_output_tokens=300)
+            model="gemini-1.5-flash-001", max_output_tokens=300, examples=EXAMPLES
+        )
+
+    def get_system_prompt(self) -> str:
+        return self.system_context
 
     def chat(self, agent_state) -> AgentResponse:
-        promptTemplate = self._from_messages(SYSTEM_PROMPT, agent_state)
-        prompt = promptTemplate.invoke({"articles": self.article_content}, )
+
+        messages = [
+            SystemMessage(content=self.system_context),
+            *agent_state.message_history,
+            agent_state.message,
+        ]
 
         # Invoke the model with the prompt.
-        response = self.chat_model.invoke(prompt)
+        response = self.chat_model.invoke(messages)
         return (response.content, {})
